9224cce643233790ccc6a0d4e788692a5e9f5f20
JeiversonChristian <jeiversonc@gmail.com>
2023-04-04 15:38:44 -0300

    added icon and color sidebar on the code

--- main.py
+++ main.py
@@ -7,9 +7,26 @@ from tkinter import *
 # import tkinter as tk
 
 whiteboard  = Tk()
+
+# creating the whiteboard window
 whiteboard.title("Whiteboard")
 whiteboard.geometry("1050x570+150+50")
 whiteboard.configure(bg="#ffffff") # ffffff -> hexadecimal to white color
+
+# locking the sizes dimensions of the whiteboard
 whiteboard.resizable(False, False) # Width and Height
+
+# icon
+whiteboard.iconbitmap(default="icon/whiteboard.ico")
+#board_icon = PhotoImage(file="icon/whiteboard.ico")
+
+# image
+color_sidebar_image = PhotoImage(file="images/color_sidebar.png")
+
+# label
+color_sidebar_label = Label(whiteboard, image=color_sidebar_image, bg="#ffffff")
+
+# Placing label
+color_sidebar_label.place(x=10, y=15)
 
 whiteboard.mainloop()
