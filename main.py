ead91bf8582902aceafe7f1f2ce23e10c2cee232
JeiversonChristian <jeiversonc@gmail.com>
2023-04-05 15:26:43 -0300

    creating a canvas to place a colour palette

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ whiteboard  = Tk()
 # creating the whiteboard window
 whiteboard.title("Whiteboard")
 whiteboard.geometry("1050x570+150+50")
-whiteboard.configure(bg="#ffffff") # ffffff -> hexadecimal to white color
+whiteboard.configure(bg="#f9f9f9") # f8f8f8 -> hexadecimal to a shade of white color
 
 # locking the sizes dimensions of the whiteboard
 whiteboard.resizable(False, False) # Width and Height
@@ -24,9 +24,13 @@ whiteboard.iconbitmap(default="icon/whiteboard.ico")
 color_sidebar_image = PhotoImage(file="images/color_sidebar.png")
 
 # label
-color_sidebar_label = Label(whiteboard, image=color_sidebar_image, bg="#ffffff")
+color_sidebar_label = Label(whiteboard, image=color_sidebar_image, bg="#f8f8f8")
 
 # Placing label
 color_sidebar_label.place(x=10, y=15)
+
+# Canvas
+colour_palette = Canvas(whiteboard, bg="#ffffff", width=50, height=290, bd=0)
+colour_palette.place(x=20, y=40)
 
 whiteboard.mainloop()
