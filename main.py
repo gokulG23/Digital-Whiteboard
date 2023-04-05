9c8b66cdb016603316469624fb2b409f50565d91
JeiversonChristian <jeiversonc@gmail.com>
2023-04-05 15:45:11 -0300

    creating eraser button

--- main.py
+++ main.py
@@ -18,19 +18,22 @@ whiteboard.resizable(False, False) # Width and Height
 
 # icon
 whiteboard.iconbitmap(default="icon/whiteboard.ico")
-#board_icon = PhotoImage(file="icon/whiteboard.ico")
 
-# image
+# images
 color_sidebar_image = PhotoImage(file="images/color_sidebar.png")
+eraser_image = PhotoImage(file="images/eraser.png")
 
 # label
 color_sidebar_label = Label(whiteboard, image=color_sidebar_image, bg="#f8f8f8")
-
-# Placing label
 color_sidebar_label.place(x=10, y=15)
 
 # Canvas
 colour_palette = Canvas(whiteboard, bg="#ffffff", width=50, height=290, bd=0)
 colour_palette.place(x=20, y=40)
+
+#buttons
+eraser_button = Button(whiteboard, image=eraser_image, bg="#f2f3f5")
+eraser_button.place(x=20, y=340)
+
 
 whiteboard.mainloop()
